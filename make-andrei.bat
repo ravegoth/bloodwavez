a1202ba5701aa081c5c4881f187a538e95165564
Andrei <andrei@seniuc.ro>
2025-03-19 21:59:58 +0200

    comanda de rulare

---
+++ make-andrei.bat
@@ -0,0 +1,45 @@
+echo off
+cls
+
+rem INCHIDEREA PROCESULUI PRECEDENT ##############
+
+taskkill /f /im bloodwavez.exe >nul
+
+if %ERRORLEVEL% EQU 0 (
+    echo Previous process closed
+) else (
+    @REM echo Failed to close previous process
+    cls
+)
+
+echo Compiling...
+
+rem COMPILAREA PROIECTULUI #######################
+
+g++ src/main.cpp -o bin/bloodwavez.exe ^
+-I "C:\SFML\SFML-3.0.0-windows-gcc-14.2.0-mingw-64-bit\SFML-3.0.0\include" ^
+-L "C:\SFML\SFML-3.0.0-windows-gcc-14.2.0-mingw-64-bit\SFML-3.0.0\lib" ^
+-lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network -mwindows
+
+rem EXECUTAREA PROIECTULUI #######################
+
+if %ERRORLEVEL% EQU 0 (
+    echo Compilation successful
+    echo Running...
+    bin\bloodwavez.exe
+    echo Game closed
+) else (
+    echo Compilation failed
+    pause
+)
+
+rem STERGERE EXECUTABIL #######################
+
+del bin\bloodwavez.exe -f
+
+if %ERRORLEVEL% EQU 0 (
+    echo Executable deleted
+) else (
+    echo Failed to delete executable
+    pause
+)
